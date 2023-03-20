5eb9f9078b8bff9aa3be12d5d71bb6ed75ddafc1
Ryan Hood <114943155+ryanhood10@users.noreply.github.com>
2023-03-20 17:33:33 -0500

    added productTag model

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,12 +5,19 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category);
 // Categories have many Products
-
-// Products belongToMany Tags (through ProductTag)
-
+Category.hasMany(Product);
+// Products belongToMany Tags (through ProductTag) pushing to product-tag 
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: 'product_id',
+});
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: 'tag_id',
+});
 
 module.exports = {
   Product,
